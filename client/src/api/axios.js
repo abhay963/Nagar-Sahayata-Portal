f737e3734b64a30c501b0aa28d5932cae506cc20
abhay963 <abhayyadav96312@gmail.com>
2026-08-26 17:01:43 +0530

    refactor: simplify axios instance configuration by using VITE_API_URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,20 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000"
-      : "https://nagar.up.railway.app",
-
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
-
 instance.interceptors.request.use(
-
   (config) => {
-
     const token = localStorage.getItem("token");
 
     if (token) {
@@ -23,7 +15,6 @@ instance.interceptors.request.use(
 
     return config;
   },
-
   (error) => Promise.reject(error)
 );
 
