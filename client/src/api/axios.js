4697affba1285ae43af939d34f1829675a2f6060
abhay963 <abhayyadav96312@gmail.com>
2026-08-26 17:13:13 +0530

    fix: update axios baseURL configuration for development and production environments

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? import.meta.env.VITE_API_URL
+      : "/api",
+
   withCredentials: true,
 });
 
